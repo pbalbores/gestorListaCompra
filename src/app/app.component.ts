1abf6542620bd4bece6873fba12a022981c0f174
pbalbores <56827765+pbalbores@users.noreply.github.com>
2020-02-15 13:50:22 +0100

    Pintamos

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,18 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Tarea } from './models/compraslist';
+import { TareaServiceTsService } from './services/tarea.service.ts.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'Gestor Lista Compra';
 
   tareas: Tarea[];
 
-  constructor() {
+  constructor(private tareaServiceTsService: TareaServiceTsService) {
     // this.tareas = []
   }
-
+  ngOnInit() {
+    this.tareas = this.tareaServiceTsService.getTareas();
+  }
 }
